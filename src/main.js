bfb97d70559f52e1b3567ae28e8cc4816a9c74e1
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-23 03:08:50 +0300

    Add first part of the about us page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import HeroSection from './HeroSection.vue'
+import AboutUs from './AboutUs.vue'
 
-createApp(HeroSection).mount('#HeroSection')
+createApp(HeroSection).mount('#HeroSection');
+createApp(AboutUs).mount('#AboutUsSection');
