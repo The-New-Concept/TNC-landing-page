8297fa7830f81c890821cdf3a4c511a970cf6315
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-23 19:52:19 +0300

    Add products section

---
+++ src/ProductsSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-orange h-auto place-content-center p-10">
+    <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Products</h1>
+    <div class="mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-8 gap-8" >
+      <div>
+        <div class="product-card">
+          <h2>Tank War: First Real Life Tank War</h2>
+          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
+        </div>
+      </div>
+      <div>
+        <div class="product-card">
+          <h2>Battle Bumper</h2>
+          <p> The New Concept is not just about creating entertainment; it's about crafting experiences that resonate, inspire, and bring people together in new and exciting ways.</p>
+        </div>
+      </div>
+      <div>
+        <div class="product-card">
+          <h2>Smart Archery Tag</h2>
+          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
+        </div>
+      </div>
+      <div>
+        <div class="product-card">
+          <h2>Warrior tag</h2>
+          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
+        </div>
+      </div>
+      <div>
+        <div class="product-card">
+          <h2>Digital Shooting Range</h2>
+          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
+        </div>
+      </div>
+      <div>
+        <div class="product-card">
+          <h2>Digital Archery Range</h2>
+          <p>A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import HeroSection from './HeroSection.vue'
 import AboutUs from './AboutUs.vue'
 import ValuesSection from './ValuesSection.vue'
 import StrategicFocusAreasSection from './StrategicFocusAreasSection.vue'
+import ProductsSection from './ProductsSection.vue'
 
 createApp(HeroSection).mount('#HeroSection');
 createApp(AboutUs).mount('#AboutUsSection');
 createApp(ValuesSection).mount('#ValuesSection');
 createApp(StrategicFocusAreasSection).mount('#StrategicFocusAreasSection');
+createApp(ProductsSection).mount('#ProductsSection');
