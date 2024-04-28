c2353543a429e9efba9a5dbe429ef5f0feada8fb
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-28 23:29:20 +0300

    Change products BG and create dynamic products items

--- src/ProductsSection.vue
+++ src/ProductsSection.vue
@@ -1,42 +1,56 @@
+<script setup>
+const productsHome = [
+  {
+    name: "Tank War: First Real Life Tank War",
+    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+  {
+    name: "Battle Bumper",
+    description: "The New Concept is not just about creating entertainment; it's about crafting experiences that resonate, inspire, and bring people together in new and exciting ways.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+  {
+    name: "Smart Archery Tag",
+    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+  {
+    name: "Warrior tag",
+    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+  {
+    name: "Digital Shooting Range",
+    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+  {
+    name: "Digital Archery Range",
+    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
+    link: "https://www.npmjs.com/",
+    image: "/images/npm.png",
+  },
+]
+</script>
+
 <template>
-  <div class="bg-orange h-auto place-content-center p-10">
+  <div class="relative isolate overflow-hidden bg-[url('assets/Hero-bg.png')] h-auto place-content-center p-10">
+
     <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Products</h1>
     <div class="mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-8 gap-8" >
-      <div>
-        <div class="product-card">
-          <h2>Tank War: First Real Life Tank War</h2>
-          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
-        </div>
-      </div>
-      <div>
-        <div class="product-card">
-          <h2>Battle Bumper</h2>
-          <p> The New Concept is not just about creating entertainment; it's about crafting experiences that resonate, inspire, and bring people together in new and exciting ways.</p>
-        </div>
-      </div>
-      <div>
-        <div class="product-card">
-          <h2>Smart Archery Tag</h2>
-          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
-        </div>
-      </div>
-      <div>
-        <div class="product-card">
-          <h2>Warrior tag</h2>
-          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
-        </div>
-      </div>
-      <div>
-        <div class="product-card">
-          <h2>Digital Shooting Range</h2>
-          <p> A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
-        </div>
-      </div>
-      <div>
-        <div class="product-card">
-          <h2>Digital Archery Range</h2>
-          <p>A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.</p>
-        </div>
+      <div v-for="product in productsHome" >
+        <a :key="product.name" href="#" class="">
+          <div class="product-card" >
+            <h2 class="text-lg font-semibold">{{product.name}}</h2>
+            <p>{{ product.description }}</p>
+          </div>
+        </a>
       </div>
     </div>
   </div>
