ae4399d20e8875f1c9efa309f9ef5f983aa5c03b
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 00:27:39 +0300

    Add images for the products

--- src/ProductsSection.vue
+++ src/ProductsSection.vue
@@ -6,31 +6,25 @@ const productsHome = [
     name: "Tank War: First Real Life Tank War",
     description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
     link: "https://www.npmjs.com/",
-    image: "tank-war.png",
+    image: "war-tank.jpeg",
   },
   {
     name: "Battle Bumper",
     description: "The New Concept is not just about creating entertainment; it's about crafting experiences that resonate, inspire, and bring people together in new and exciting ways.",
     link: "https://www.npmjs.com/",
-    image: "battle-bumber.png",
+    image: "battle-bumper.jpeg",
   },
   {
     name: "Smart Archery Tag",
     description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
     link: "https://www.npmjs.com/",
-    image: "tank-war.png",
+    image: "smart-archery.jpg",
   },
   {
     name: "Warrior tag",
     description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
     link: "https://www.npmjs.com/",
-    image: "tank-war.png",
-  },
-  {
-    name: "Digital Shooting Range",
-    description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
-    link: "https://www.npmjs.com/",
-    image: "tank-war.png",
+    image: "warrior-tag.jpg",
   },
   {
     name: "Digital Archery Range",
@@ -46,11 +40,11 @@ const productsHome = [
   <div class="relative isolate overflow-hidden bg-[url('assets/Hero-bg.png')] h-auto place-content-center p-10 border-none bg-center bg-no-repeat bg-cover">
 
     <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Products</h1>
-    <div class="mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-8 gap-y-8 gap-x-10" >
-      <div v-for="product in productsHome" :key="product.name" class="product-card border-none" >
-        <a  href="#" class="">
-          <div class="overflow-hidden box-content h-1/2">
-            <img :src="'src/assets/' + product.image" :alt="product.name" class="transition-all hover:scale-125"/>
+    <div class="mt-10 grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-8 gap-y-8 gap-x-10 max-h-screen" >
+      <div v-for="product in productsHome" :key="product.name" class="product-card border-none box-content" >
+        <a  href="#" class="h-10 overflow-hidden ">
+          <div class="overflow-hidden box-content h-1/2 max-h-1/2 flex ">
+            <!--<img :src="'src/assets/' + product.image" :alt="product.name" class="transition-all h-full w-full hover:scale-105 place-self-center"/>-->
           </div>
           <div class="p-10" >
             <h2 class="text-xl font-semibold">{{product.name}}</h2>
