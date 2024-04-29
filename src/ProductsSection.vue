bd31773a6c681cbda981162543cc1a1a0796086d
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 01:13:15 +0300

    Finalize products section layout

--- src/ProductsSection.vue
+++ src/ProductsSection.vue
@@ -30,25 +30,24 @@ const productsHome = [
     name: "Digital Archery Range",
     description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
     link: "https://www.npmjs.com/",
-    image: "tank-war.png",
-    bgClass: "bg-[url('assets/IMG_9197.JPG')]"
+    image: "archery.jpg",
   },
 ]
 </script>
 
 <template>
-  <div class="relative isolate overflow-hidden bg-[url('assets/Hero-bg.png')] h-auto place-content-center p-10 border-none bg-center bg-no-repeat bg-cover">
+  <div class="bg-[url('assets/Hero-bg.png')] p-10 border-none bg-center bg-no-repeat bg-cover">
 
     <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Products</h1>
-    <div class="mt-10 grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-8 gap-y-8 gap-x-10 max-h-screen" >
+    <div class="mt-10 grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-8 gap-4 max-h-screen" >
       <div v-for="product in productsHome" :key="product.name" class="product-card border-none box-content" >
         <a  href="#" class="h-10 overflow-hidden ">
-          <div class="overflow-hidden box-content h-1/2 max-h-1/2 flex ">
-            <!--<img :src="'src/assets/' + product.image" :alt="product.name" class="transition-all h-full w-full hover:scale-105 place-self-center"/>-->
+          <div class="overflow-hidden box-content h-1/3 max-h-1/3">
+            <img :src="'src/assets/' + product.image" :alt="product.name" class="transition-all h-full w-full hover:scale-105 place-self-center"/>
           </div>
-          <div class="p-10" >
+          <div class="py-10 px-5" >
             <h2 class="text-xl font-semibold">{{product.name}}</h2>
-            <p class="text-sm">{{ product.description }}</p>
+            <p class="text-sm text-justify">{{ product.description }}</p>
           </div>
         </a>
       </div>
