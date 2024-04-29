ac6b21d95933a136d35fa9370725ef21228e971c
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-29 15:36:50 +0300

    Styling the productsSection

--- src/ProductsSection.vue
+++ src/ProductsSection.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {ref} from "vue";
+
 const productsHome = [
   {
     name: "Tank War: First Real Life Tank War",
@@ -35,6 +37,7 @@ const productsHome = [
     description: "A groundbreaking tank warfare simulation game that combines historical accuracy, realistic gameplay, and advanced multiplayer features to offer an unparalleled gaming experience.",
     link: "https://www.npmjs.com/",
     image: "/images/npm.png",
+    bgClass: "bg-[url('assets/IMG_9197.JPG')]"
   },
 ]
 </script>
@@ -44,9 +47,9 @@ const productsHome = [
 
     <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Products</h1>
     <div class="mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-8 gap-8" >
-      <div v-for="product in productsHome" >
-        <a :key="product.name" href="#" class="">
-          <div class="product-card" >
+      <div v-for="product in productsHome" :key="product.name" class="product-card border-none bg-[url('assets/IMG_9197.JPG')]" >
+        <a  href="#">
+          <div class="" >
             <h2 class="text-lg font-semibold">{{product.name}}</h2>
             <p>{{ product.description }}</p>
           </div>
