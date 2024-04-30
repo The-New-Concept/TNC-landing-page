4cdee6c50771bdccf83ca534b9f16ad0b23ea512
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 18:39:38 +0300

    Test5

--- src/ContactForm.vue
+++ src/ContactForm.vue
@@ -5,65 +5,41 @@ import ProductsSection from "./components/ProductsSection.vue";
 import {CheckIcon, EyeIcon} from "@heroicons/vue/20/solid/index.js";
 
 </script>
-<template>
-  <Header/>
-  <div class="hero-section bg-[url('assets/Hero-bg.png')] relative isolate overflow-hidden te px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-
-    <!-- BG Pattern-->
-    <div class="absolute inset-0 -z-10 overflow-hidden">
-      <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-orange [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
-        <defs>
-          <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-            <path d="M100 200V.5M.5 .5H200" fill="none" />
-          </pattern>
-        </defs>
-
-        <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-      </svg>
-    </div>
-    <!-- /BG Pattern-->
-
-    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-      <form name="contact" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-
-      <!-- Side Image -->
-      <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden flex justify-center lg:gap-x-8 lg:px-8">
-        <img class="w-[20rem] sm:w-[30rem]" src="./assets/TNC-Logo.png" alt="" />
-      </div>
-      <!-- /Side Image -->
-
-      <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-        <div class="lg:pr-4">
-          <div class="max-w-xl text-base leading-7 text-gray-200 lg:max-w-lg">
-            <ul role="list" class="mt-8 space-y-8 text-gray-200">
-              <li class="flex gap-x-3">
-                <EyeIcon class="mt-1 h-5 w-5 flex-none text-gray-50" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-100">Our Vision</strong> Is too be at the forefront of the entertainment industry, constantly pushing the boundaries of creativity and technology to provide novel entertainment options that captivate and inspire audiences worldwide.
-</span>
-              </li>
-              <li class="flex gap-x-3">
-                <CheckIcon class="mt-1 h-5 w-5 flex-none text-gray-50" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-100">Our Mission</strong> Is to innovate and redefine the standards of entertainment through the development of cutting-edge games and experiences that appeal to a broad spectrum of audiences, thereby enhancing the global entertainment landscape.
-</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+>
+<input type="hidden" name="form-name" value="ask-question" />
+<label v-for="(panelist, index) in panelists" :key="index">
+  <input
+      type="radio"
+      name="panelist"
+      :value="panelist"
+      @input="ev => updatePanelist"
+      :checked="panelist === currentPanelist"
+  />
+  <span>{{ panelist }}</span>
+</label>
+...
+<button>Submit</button>
+</form>
 </template>
+<script>
+export default {
+  name: "QAForm",
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  },
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  }
+}
+</script>
 
