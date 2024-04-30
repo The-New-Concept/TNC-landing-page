277af2bd7a18c039fc13393ef5aca89120a41228
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 18:25:28 +0300

    Test2

--- src/ContactForm.vue
+++ src/ContactForm.vue
@@ -24,6 +24,17 @@ import {CheckIcon, EyeIcon} from "@heroicons/vue/20/solid/index.js";
     <!-- /BG Pattern-->
 
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
       <form action="" netlify class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="lg:max-w-lg">
