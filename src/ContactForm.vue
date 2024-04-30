6f4c2bd1ebfe113925ae2db5972eb2cdc497c589
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 18:29:18 +0300

    Test3

--- src/ContactForm.vue
+++ src/ContactForm.vue
@@ -24,24 +24,25 @@ import {CheckIcon, EyeIcon} from "@heroicons/vue/20/solid/index.js";
     <!-- /BG Pattern-->
 
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-      <form name="contact" netlify>
+      <form name="contact" method="POST" data-netlify="true">
         <p>
-          <label>Name <input type="text" name="name" /></label>
+          <label>Your Name: <input type="text" name="name" /></label>
         </p>
         <p>
-          <label>Email <input type="email" name="email" /></label>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
-      </form>
-      <form action="" netlify class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-        <div class="lg:pr-4">
-          <div class="lg:max-w-lg">
-            <input type="text" name="Name" id="name">
-            <input type="submit" value="Send">
-          </div>
-        </div>
       </form>
 
       <!-- Side Image -->
