00e0dfb68c07d0763d67ad364a85fdb38b0e5237
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2024-04-30 18:58:59 +0300

    Test8

--- src/ContactForm.vue
+++ src/ContactForm.vue
@@ -1,42 +1,34 @@
-<script setup>
-import {ref} from "vue";
-import Header from "./components/Header.vue";
-import ProductsSection from "./components/ProductsSection.vue";
-import {CheckIcon, EyeIcon} from "@heroicons/vue/20/solid/index.js";
-
-</script>
 <template>
-<form
-    name="Contact"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
->
-<input type="hidden" name="form-name" value="Contact" data-netlify="true"
-       data-netlify-honeypot="bot-field"/>
-<label v-for="(panelist, index) in panelists" :key="index">
-  <input
-      type="radio"
-      name="panelist"
-      :value="panelist"
-      @input="ev => updatePanelist"
-      :checked="panelist === currentPanelist"
-  />
-  <span>{{ panelist }}</span>
-</label>
-...
-<button>Submit</button>
-</form>
+  <form
+      name="ask-question"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+          type="radio"
+          name="panelist"
+          :value="panelist"
+          @input="ev => updatePanelist"
+          :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
 <script>
 export default {
   name: "QAForm",
   methods: {
-    updatePanelist (ev) {
+    updatePanelist(ev) {
       this.currentPanelist = ev.target.value
     }
   },
-  data () {
+  data() {
     return {
       panelists: ['Evan You', 'Chris Fritz'],
       currentPanelist: 'Evan You'
@@ -44,4 +36,3 @@ export default {
   }
 }
 </script>
-
